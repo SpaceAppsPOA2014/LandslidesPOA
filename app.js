a3a678492369698b03c0f108154cb05b5c104751
romulobr <romulobr@gmail.com>
2014-04-12 18:37:16 -0300

    add .gitignore

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 var express = require('express'),
-	app = express();
+	app = express(),
+	mongoDb = require('mongodb').Db;
+
+var SlideReportProvider = require('./lib/providers/slideReportProvider.js'),
+    SlideReport = require('./lib/slideReport.js'),
+    slideReports = SlideReportProvider.create(mongoDb);
 
 app.use(express.static('./public'));
 
+app.get('/save', function(req, res) {
+  var geoLocation = {latitude:1000, longitude:2000},
+  	  hazzardLevel = 2,
+  	  images = ['anImage','otherImage'],
+  	  created_at = new Date();
+
+  slideReports.save(SlideReport.create(geoLocation, hazzardLevel, images, created_at));
+  res.send('boa bonitão!');
+});
+
 app.listen(3000);
+
+
+
