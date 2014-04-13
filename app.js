1276c7d911aeb78b55fbd583e4c0ccecd17d35d3
romulobr <romulobr@gmail.com>
2014-04-13 02:29:02 -0300

    adds upload to cloudinary, coudinary api key must be added as an envirounment variable.

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 var express = require('express'),
 	app = express(),
 	mongoDb = require('mongodb').Db;
-  Server = require('mongodb').Server;
+  Server = require('mongodb').Server,
+  multipart = require('connect-multiparty'),
+  cloudinary = require('cloudinary');
+
+var multipartMiddleware = multipart();
 
 var SlideReportProvider = require('./lib/providers/slideReportProvider.js'),
     SlideReport = require('./lib/slideReport.js'),
     slideReports = SlideReportProvider.create(mongoDb, Server);
 
+cloudinary.config({
+  cloud_name: 'landslidepoa',
+  api_key: '884447717328561',
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
 app.use(express.static('./public'));
+
+app.post('/upload', multipartMiddleware, function(req, res) {
+  cloudinary.uploader.upload(req.files.file.path, function(result) {
+    res.send({imageUrl: result.url});
+  });
+});
 
 app.get('/save', function(req, res) {
   var geoLocation = {latitude:1000, longitude:2000},
